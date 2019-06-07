7673adc516c88c7cf59c7e044c3ac2714d73562f
DESKTOP-3A4BHLQ\junic <junicolas@gmail.com>
2019-06-07 22:53:42 +0200

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,33 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FileDropComponent } from './file-drop/file-drop.component';
+import { NgxFileDropModule } from 'ngx-file-drop';
+import {HttpClientModule} from "@angular/common/http";
+import {ConfigService} from './config.service';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatCard, MatCardModule, MatIconModule, MatToolbarModule} from "@angular/material";
+import {FormsModule} from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FileDropComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    NgxFileDropModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatIconModule,
+    MatToolbarModule
   ],
-  providers: [],
+  providers: [ConfigService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
